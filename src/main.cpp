d069f06910711c3bd51122fab4629aedf85ae589
UseAfterFreee <wesselinkbram2@gmail.com>
2018-12-13 13:39:00 +0100

    Basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
+#include "window.hpp"
+#include "event_handler.hpp"
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    window window(1000, 1000);
+    EventHandler eh;
+
+    window.createWindow();
+
+    while (!eh.needQuit())
+    {
+        eh.handleEvents();
+        window.drawBackground(255,255,255);
+
+        SDL_RenderPresent(window.getRenderer());
+        SDL_Delay(1000/60.0);
+    }
 }
