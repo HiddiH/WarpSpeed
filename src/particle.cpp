4c7d426dee9fc5750d6db6371447c7de3bcb13c0
Peter K <peterjgkdoff@gmail.com>
2018-12-18 15:33:16 +0100

    Superior programming

--- src/particle.cpp
+++ src/particle.cpp
@@ -21,10 +21,19 @@ void Particle::tick(float t)
     vel = dir * (0.5/(1+exp(-4*(t-3)))+0.0001);
 
     /* Update position based on velocity */
+    Vec2D OldPos = pos;
     pos += vel;
 
     acc.x = 0;
     acc.y = 0;
+
+    if (pos.x < 0 || pos.x > 1000) {
+        pos = OldPos;
+    }
+
+    if (pos.y < 0 || pos.y > 1000) {
+        pos = OldPos;
+    }
 }
 
 void Particle::applyForce(float x, float y)
